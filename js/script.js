da5efa048dd996f219879f200968a2f77330eed4
Ricardo <ricardomg92@hotmail.com>
2021-11-30 21:53:02 +0100

    Increased the number of squares to 16x16 and starting code to align all the squares inside the sketchPad div

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ document.body.onload = addSquare;
 
 function addSquare() {
     // Loops 16 times to create 16 square divs
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 256; i++) {
+
+        const squareSize = 500/16;
+
         // Creates a new div with a black background and 10px of width and heigth 
         const square = document.createElement("div");
         square.style.backgroundColor = "white";
         square.style.borderColor = "black";
         square.style.borderStyle = "solid";
-        //square.style.width = "10px";
-        //square.style.height = "10px";
+        square.style.borderWidth = "1px"
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
 
         //inserts the newly created div on the sketchPad div
         const sketchPad = document.getElementById("sketchPad");
