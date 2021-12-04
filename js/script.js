cb11ac2e3721aab3e26058223947120fba6d2bc0
Ricardo <ricardomg92@hotmail.com>
2021-12-04 11:03:44 +0100

    First complete working version of the sketchpad

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
-document.body.onload = addSquare;
+document.body.onload = askForSquares;
 
-function addSquare() {
+const sketchPad = document.getElementById("sketchPad");
+const square = document.getElementsByClassName("square");
+
+// resets the drawing when clicking the reset button
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", function () {
+    cleanPad();
+    askForSquares();
+})
+
+function askForSquares() {
+    let squaresPerSide = Number(window.prompt("How many squares per size do you want?", ""));
+    let totalSquares = squaresPerSide * squaresPerSide;
+    addSquare(squaresPerSide, totalSquares);
+    paintSquares(totalSquares);
+}
+
+function addSquare(squaresPerSide, totalSquares) {
     // Loops 16 times to create 16 square divs
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < totalSquares; i++) {
+        const squareSize = 500 / squaresPerSide;
 
-        const squareSize = 500 / 16;
-
-        // Creates a new div with a black background and 10px of width and heigth 
+        // Creates a new div with a white background and 10px of width and heigth 
         const square = document.createElement("div");
         square.style.backgroundColor = "white";
         square.style.borderColor = "black";
@@ -16,21 +32,22 @@ function addSquare() {
         square.style.height = `${squareSize}px`;
         square.className = "square"
 
-        //inserts the newly created div on the sketchPad div
-        const sketchPad = document.getElementById("sketchPad");
+        //inserts the newly created div on the sketchPad div        
         sketchPad.insertAdjacentElement("afterbegin", square);
-
     }
-    paintSquares();
 }
 
-function paintSquares() {
-    // gets all the small squares
-    const squareToPaint = document.getElementsByClassName("square");
+function paintSquares(totalSquares) {
     // and loops trough them adding an eventListener for mouse hover
-    for (let i = 0; i < 256; i++) {
-        squareToPaint[i].addEventListener("mouseover", function () {
+    for (let i = 0; i < totalSquares; i++) {
+        square[i].addEventListener("mouseover", function () {
             this.style.backgroundColor = "black";
         });
+    }
+}
+
+function cleanPad() {
+    while (sketchPad.firstChild) {
+        sketchPad.removeChild(sketchPad.firstChild);
     }
 }
